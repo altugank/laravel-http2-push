0f9106b4293d603da6235f1c00cc24d5d87d68d2
Altug Ankarali <iletisim@altugankarali.com>
2020-11-06 01:01:46 +0300

    Update http2push.php
    Call toHmlt() on preload mix url in order to make config:cache work.

--- config/http2push.php
+++ config/http2push.php
@@ -6,10 +6,10 @@ return [
      * The resources which should be preloaded on every get request.
      */
     'preload' => [
-        // mix('css/app.css'),
-        // mix('js/manifest.js'),
-        // mix('js/vendor.js'),
-        // mix('js/app.js')
+        // mix('css/app.css')->toHtml(),
+        // mix('js/manifest.js')->toHtml(),
+        // mix('js/vendor.js')->toHtml(),
+        // mix('js/app.js')->toHtml()
     ],
 
 ];
